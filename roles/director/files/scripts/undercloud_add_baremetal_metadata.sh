a6964ed2a9721342ad4f7e616e89c6e6fd5b9a6f
Thibaut Lapierre <tlapierr@redhat.com>
2019-10-17 11:10:32 +0200

    Work on Overcloud deploy

--- roles/director/files/scripts/undercloud_add_baremetal_metadata.sh
+++ roles/director/files/scripts/undercloud_add_baremetal_metadata.sh
@@ -8,17 +8,23 @@ do
     *ceph* ) 
       openstack baremetal node set --property capabilities='profile:ceph-storage,boot_option:local' $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
       root_dev="$(cat ~stack/introspection/$i.json |jq '.inventory.disks[0].serial')"
-      openstack baremetal node set --property root_device="{\"serial\": $root_dev }" $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
+      if [ "$root_dev" != null ]; then
+        openstack baremetal node set --property root_device="{\"serial\": $root_dev }" $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
+      fi
     ;;
     *control* ) 
       openstack baremetal node set --property capabilities='profile:control,boot_option:local' $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
       root_dev="$(cat ~stack/introspection/$i.json |jq '.inventory.disks[0].serial')"
-      openstack baremetal node set --property root_device="{\"serial\": $root_dev }" $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
+      if [ "$root_dev" != null ]; then
+        openstack baremetal node set --property root_device="{\"serial\": $root_dev }" $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
+      fi
     ;;
     *compute* ) 
       openstack baremetal node set --property capabilities='profile:compute,boot_option:local' $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
       root_dev="$(cat ~stack/introspection/$i.json |jq '.inventory.disks[0].serial')"
-      openstack baremetal node set --property root_device="{\"serial\": $root_dev }" $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
+      if [ "$root_dev" != null ]; then
+        openstack baremetal node set --property root_device="{\"serial\": $root_dev }" $(openstack baremetal node list -f value -c Name -c UUID |grep $i |awk '{print $1}')
+      fi
     ;;
     * ) echo "Error...";;
   esac
