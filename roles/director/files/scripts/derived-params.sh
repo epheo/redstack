e14239cbd87652929b4e4311fe7df657ad946e03
Thibaut Lapierre <tlapierr@redhat.com>
2019-11-04 23:35:31 +0100

    Refacto Environments names
    Add First-Boot
    Remove wipe-disk

--- roles/director/files/scripts/derived-params.sh
+++ roles/director/files/scripts/derived-params.sh
@@ -7,10 +7,10 @@ openstack overcloud deploy \
   -r ~/templates/roles_data.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
-  -e ~/templates/environments/10-deployment-environment.yaml \
-  -e ~/templates/environments/40-network-override-environment.yaml \
-  -e ~/templates/environments/99-extraconfig-environment.yaml \
-  -e ~/templates/environments/30-storage-environment.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/host-config-and-reboot.yaml \
+  -e ~/templates/environments/99-extra.yaml \
+  -e ~/templates/environments/10-global.yaml \
+  -e ~/templates/environments/40-network.yaml \
+  -e ~/templates/environments/30-ceph.yaml \
   -e ~/templates/overcloud_images.yaml \
   -p /usr/share/openstack-tripleo-heat-templates/plan-samples/plan-environment-derived-params.yaml
