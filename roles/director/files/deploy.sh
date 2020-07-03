ca22f61c4d3c7211dd5247bb5e56c14f3ef7279b
Thibaut Lapierre <tlapierr@redhat.com>
2020-07-04 01:36:01 +0200

    WIP

--- roles/director/files/deploy.sh
+++ roles/director/files/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+source stackrc
+
 time openstack overcloud deploy \
 -n ~/templates/network_data.yaml \
 -r ~/templates/roles_data.yaml \
---answers-file ~/answers.yaml #\
-#--stack-only
+--answers-file ~/answers.yaml \
+--log-file overcloud-install.log
